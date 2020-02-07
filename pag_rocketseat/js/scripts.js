3cf39d0ee02208ed8da38c0e225261a064b841bc
Anderson Menezes <anderson.stifip@gmail.com>
2020-02-07 05:51:00 -0200

    first commit

---
+++ pag_rocketseat/js/scripts.js
@@ -0,0 +1,26 @@
+const modalOverlay = document.querySelector(".modal-overlay")
+const modal = document.querySelector('.modal')
+const cards = document.querySelectorAll(".card")
+
+for (let card of cards) {
+  card.addEventListener("click", function() {
+    const host = card.getAttribute("id")
+    modalOverlay.classList.add("active")
+    modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${host}`
+  })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("iframe").src = ""
+    modal.classList.remove('maximize')
+})
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){ 
+  if (modal.classList.contains('maximize')){
+    modal.classList.remove('maximize')
+  }else{
+    modal.classList.add('maximize')
+  }
+})
+
